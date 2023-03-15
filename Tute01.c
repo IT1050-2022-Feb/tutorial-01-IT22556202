d2fcbe574ffa702bf3f7f38aba62ecb170bae271
IT22556202 <127476001+IT22556202@users.noreply.github.com>
2023-03-15 08:38:07 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+   int mark1,mark2,avg;
+   
+   printf("Enter the mark of subject 1:");
+   scanf("%d",&mark1);
+   
+   printf("Enter the mark of subject 2:");
+   scanf("%d",&mark2);
+   
+   avg=(mark1+mark2)/2;
+   
+   printf("The average of the 2 marks:%d",avg);
+   
   
   return 0;
 }
